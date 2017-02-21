a53445acbd39c78b71a7aae6a67c43d2f3870390
191576565 <191576565@qq.com>
2017-02-21 10:55:24 +0800

    添加登录后台逻辑代码

--- AM/src/main/java/com/tianjian/auth/mvc/login/LoginService.java
+++ AM/src/main/java/com/tianjian/auth/mvc/login/LoginService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.tianjian.auth.mvc.login;
 
 import com.tianjian.auth.mvc.base.BaseSecurityMD5;
@@ -46,37 +45,3 @@ public class LoginService {
 		}
 	}
 }
-=======
-package com.tianjian.auth.mvc.login;
-
-import java.util.List;
-
-import com.jfinal.plugin.activerecord.Db;
-import com.jfinal.plugin.activerecord.Record;
-import com.tianjian.auth.mvc.login.Login;
-import com.tianjian.platform.tools.ToolBase64;
-import com.tianjian.platform.tools.ToolGetSql;
-
-public class LoginService {
-
-	public boolean validUser(String username, String password) {
-		return true;
-/*              test
-		String sql = ToolGetSql.getSqlByDbtype("tianjian.login.ucheck");
-
-		List<Record> loginuser = Db.find(sql, username);
-		System.out.println(loginuser);
-		
-		if (loginuser.size() != 1) {
-			return false;
-		}
-		String pass = loginuser.get(0).get("USER_PWD");
-		String depass = ToolBase64.decryptBASE64(pass);
-		if (password.equals(depass)) {
-			return true;
-		} else {
-			return false;
-		}*/
-	}
-}
->>>>>>> branch 'master' of https://github.com/191576565/Auth
