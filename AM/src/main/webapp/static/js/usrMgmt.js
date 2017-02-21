fae28484db0ca3cc8805024403dfe1c08ff31ed2
FitzFang <fcdlutfc@hotmail.com>
2017-02-21 16:56:54 +0800

    更新字段

--- AM/src/main/webapp/static/js/usrMgmt.js
+++ AM/src/main/webapp/static/js/usrMgmt.js
@@ -1,38 +1,42 @@
 $('#table').bootstrapTable({                                                                                                  
-    url: 'usrMgmt/defaultSelect',                                                                                                
- 	toolbar: '#toolbar', //工具按钮用哪个容器                                                                                          
- 	striped: true, //是否显示行间隔色                                                                                                 
+    url: 'usrMgmt/defaultSelect',
+	method: 'get', 					//请求方式（*）                                                                                                
+ 	toolbar: '#toolbar', //工具按钮用哪个容器                       
+	striped: false, 					//是否显示行间隔色
+	cache: false, 					//是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）                                                               
  	pagination: true, //是否显示分页（*）                                                                                             
  	sortable: false, //是否启用排序                                                                                                 
- 	sortOrder: "asc", //排序方式                                                                                                  
+ 	sortOrder: "asc", //排序方式                                            
+//	queryParams: 			oTableInit.queryParams, //传递参数（*）	
+	sidePagination: 		"client",   			//分页方式：client客户端分页，server服务端分页（*）                                                      
  	pageNumber:1, //初始化加载第一页，默认第一页                                                                                            
  	pageSize: 5, //每页的记录行数（*）                                                                                                 
  	pageList: [10, 25, 50, 100], //可供选择的每页的行数（*）                                                                              
  	strictSearch: true,                                                                                                       
  	showColumns: false, //是否显示所有的列                                                                                             
  	showRefresh: false, //是否显示刷新按钮                                                                                             
- 	clickToSelect: false, //是否启用点击选中行                                                                                          
+ 	clickToSelect: true, //是否启用点击选中行                                                                                          
  	uniqueId: "ID", //每一行的唯一标识，一般为主键列                                                                                         
  	showToggle:false, //是否显示详细视图和列表视图的切换按钮                                                                                     
  	cardView: false, //是否显示详细视图                                                                                               
                                                                                                                               
    columns: [ {                                                                                                                      
-        field: 'ORG_UUID',                                                                                                        
+        field: 'org_unit_id',                                                                                                        
         title: '机构编码'                                                                                                        
     },{        	
-        field: 'USER_ID',                                                                                                      
+        field: 'user_id',                                                                                                      
         title: '用户编码'                                                                                                         
     }, {                                                                                                                      
-        field: 'USER_NAME',                                                                                                    
+        field: 'user_name',                                                                                                    
         title: '用户名称'                                                                                                         
     }, {                                                                                                                      
-        field: 'ROLES',                                                                                                     
+        field: 'roles',                                                                                                     
         title: '角色名称'                                                                                                        
     }, {                                                                                                                      
-        field: 'USER_PHONE',                                                                                                    
+        field: 'user_phone',                                                                                                    
         title: '电话号码'                                                                                                         
     }, {                                                                                                                      
-        field: 'USER_EMAIL',                                                                                                   
+        field: 'user_email',                                                                                                   
         title: '电子信箱'                                                                                                        
     }                                                                                                                            
     ,                                                                                                                         
