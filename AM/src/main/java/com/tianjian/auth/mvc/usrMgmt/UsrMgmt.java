6767b9ee0c38e9178a3303e4c2ed6f4975ad4192
FitzFang <fcdlutfc@hotmail.com>
2017-02-21 14:44:31 +0800

    没用

--- AM/src/main/java/com/tianjian/auth/mvc/usrMgmt/UsrMgmt.java
+++
@@ -1,7 +0,0 @@
-package com.tianjian.auth.mvc.usrMgmt;
-
-import com.jfinal.plugin.activerecord.Model;
-
-public class UsrMgmt extends Model<UsrMgmt> {
-	public static final UsrMgmt dao = new UsrMgmt();
-}
