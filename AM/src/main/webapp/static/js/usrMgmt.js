1eb7c0060c3ff8cd1f9a8aad987d5d6befaaf799
FitzFang <fcdlutfc@hotmail.com>
2017-02-21 14:57:43 +0800

    更新路径

--- AM/src/main/webapp/static/js/usrMgmt.js
+++ AM/src/main/webapp/static/js/usrMgmt.js
@@ -1,5 +1,5 @@
 $('#table').bootstrapTable({                                                                                                  
-    url: 'static/js/demo/3-user.json',                                                                                                
+    url: 'usrMgmt/defaultSelect',                                                                                                
  	toolbar: '#toolbar', //工具按钮用哪个容器                                                                                          
  	striped: true, //是否显示行间隔色                                                                                                 
  	pagination: true, //是否显示分页（*）                                                                                             
