deb74d445802dd5533e3a7437940c98757879297
FitzFang <fcdlutfc@hotmail.com>
2017-02-22 13:43:22 +0800

    增加域、机构和角色查询

--- AM/src/main/java/com/tianjian/auth/mvc/usrMgmt/UsrMgmtService.java
+++ AM/src/main/java/com/tianjian/auth/mvc/usrMgmt/UsrMgmtService.java
@@ -1,6 +1,8 @@
 package com.tianjian.auth.mvc.usrMgmt;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
@@ -8,8 +10,27 @@ import com.tianjian.platform.tools.ToolGetSql;
 
 public class UsrMgmtService {
 	public List<Record> defaultSelect(){
-		String sql = ToolGetSql.getSql("tianjian.usrMgmt.defaultSelect");
+		String sql = ToolGetSql.getSql("tianjian.usrMgmt.defSel");
 		List<Record> list = Db.find(sql);
 		return list;
 	} 
+	public List<Record> getDomain(){
+		String sql = ToolGetSql.getSql("tianjian.usrMgmt.selDomain");
+		List<Record> list = Db.find(sql);
+		return list;
+	}
+	public List<Record> getOrg(String domainUUID){
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("domain_uuid", domainUUID);
+		String sql = ToolGetSql.getSql("tianjian.usrMgmt.selDomain", param);
+		List<Record> list = Db.find(sql);
+		return list;
+	}
+	public List<Record> getRole(String domainUUID){
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("domain_uuid", domainUUID);
+		String sql = ToolGetSql.getSql("tianjian.usrMgmt.selRole", param);
+		List<Record> list = Db.find(sql);
+		return list;
+	}
 }
