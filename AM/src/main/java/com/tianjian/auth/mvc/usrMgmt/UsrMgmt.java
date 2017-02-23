9194827310f504db7330e6d430cd6d2ec7eb9705
FitzFang <fcdlutfc@hotmail.com>
2017-02-23 16:34:13 +0800

    增加用户表model

---
+++ AM/src/main/java/com/tianjian/auth/mvc/usrMgmt/UsrMgmt.java
@@ -0,0 +1,7 @@
+package com.tianjian.auth.mvc.usrMgmt;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class UsrMgmt extends Model<UsrMgmt> {
+	public static final UsrMgmt dao = new UsrMgmt();
+}
