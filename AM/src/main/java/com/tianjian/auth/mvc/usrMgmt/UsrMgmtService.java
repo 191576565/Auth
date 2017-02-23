d34388b6f3536025ce09315eb584d7d1fcb09653
FitzFang <fcdlutfc@hotmail.com>
2017-02-23 10:37:42 +0800

    将用户展示修改为展示该用户所在域及所在机构以及下属机构的所有用户

--- AM/src/main/java/com/tianjian/auth/mvc/usrMgmt/UsrMgmtService.java
+++ AM/src/main/java/com/tianjian/auth/mvc/usrMgmt/UsrMgmtService.java
@@ -1,22 +1,31 @@
 package com.tianjian.auth.mvc.usrMgmt;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 import com.tianjian.platform.tools.ToolGetSql;
 
 public class UsrMgmtService {
-	public List<Record> defaultSelect(){
-		String sql = ToolGetSql.getSql("tianjian.usrMgmt.defaultSelect");
-		List<Record> list = Db.find(sql);
+	
+	public ArrayList<String> selectDomainOrg(String userId){
+		String sql = ToolGetSql.getSql("tianjian.usrMgmt.checkUserId");
+		List<Record> list = Db.find(sql,userId);
+		ArrayList<String> arrayList = new ArrayList<String>();
+		arrayList.add(list.get(0).getStr("domain_uuid"));
+		arrayList.add(list.get(0).getStr("org_uuid"));
+		return arrayList;
+	}
+	public List<Record> initSelect(ArrayList<?> param){
+		String sql = ToolGetSql.getSql("tianjian.usrMgmt.initSelect");
+		//0-->DOMAIN_UUID;1-->ORG_UUID
+		List<Record> list = Db.find(sql,param.get(0).toString(),param.get(1).toString());
 		return list;
-	} 
+	}
 	public boolean checkUserId(String userId){
 		String sql = ToolGetSql.getSql("tianjian.usrMgmt.checkUserId");
-		System.out.println(sql);
 		boolean chkRst = Db.find(sql, userId).isEmpty();
 		return chkRst;
 	}
