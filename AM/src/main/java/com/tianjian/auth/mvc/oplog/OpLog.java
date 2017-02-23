3a623078ff9a5b44caff7ac7fcea2996eb311137
hujian722 <hujian@pccqcpa.com.cn>
2017-02-23 14:46:39 +0800

    更新log

--- AM/src/main/java/com/tianjian/auth/mvc/oplog/OpLog.java
+++ AM/src/main/java/com/tianjian/auth/mvc/oplog/OpLog.java
@@ -1,11 +1,15 @@
 package com.tianjian.auth.mvc.oplog;
 
 import com.jfinal.plugin.activerecord.Model;
-import com.tianjian.auth.mvc.login.Login;
 
 public class OpLog extends Model<OpLog> {
 	public static final OpLog dao=new OpLog();	
-	
+	/*
+	 * 日志select语句
+	 */
 	public static final String sqlId_log_select = "tianjian.oplog.pageAllSelect";
+	/*
+	 * 日志from语句
+	 */
 	public static final String sqlId_log_selectfrom = "tianjian.oplog.pageAllFrom";
 }
