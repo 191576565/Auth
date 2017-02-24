09d4c8e0a18a706a132f7efc90f801692ac6847b
191576565 <191576565@qq.com>
2017-02-24 09:43:13 +0800

    谢涛 1/5

---
+++ AM/src/main/java/com/tianjian/auth/mvc/base/BaseController.java
@@ -0,0 +1,590 @@
+package com.tianjian.auth.mvc.base;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.jfinal.core.Controller;
+import com.jfinal.kit.StrKit;
+import com.jfinal.log.Log;
+import com.jfinal.render.JsonRender;
+import com.jfinal.upload.UploadFile;
+import com.tianjian.auth.mvc.constant.ConstantWebContext;
+import com.tianjian.auth.mvc.dto.SplitPage;
+import com.tianjian.auth.mvc.model.User;
+import com.tianjian.auth.mvc.tool.ToolWeb;
+import com.tianjian.platform.constant.ConstantInit;
+import com.tianjian.platform.constant.ConstantRender;
+import com.tianjian.platform.tools.ToolString;
+
+
+/**
+ * 公共Controller
+ * @author 董华健
+ */
+public abstract class BaseController extends Controller {
+
+	private static final Log log = Log.getLog(BaseController.class);
+
+	/**
+	 * 全局Service
+	 */
+	protected BaseService baseService;		// Service
+	
+	/**
+	 * 全局变量
+	 */
+	protected String ids;				// 主键
+	protected SplitPage splitPage;		// 分页封装
+	protected List<?> list;				// 公共list
+	//protected Syslog reqSysLog;			// 当前访问日志
+	
+	/**
+	 * 请求/WEB-INF/下的视图文件
+	 */
+	public void toUrl() {
+		String toUrl = getPara(ConstantWebContext.request_toUrl);
+		render(toUrl);
+	}
+
+	/**
+	 * 设置日志描述
+	 * @param description
+	 */
+	protected void setLogDesc(String description){
+		log.debug("设置日志描述：" + description);
+//		reqSysLog.set(Syslog.column_description, description);
+	}
+
+	/**
+	 * 获取当前国际化标示
+	 * @return
+	 */
+	protected String getI18nLocalePram() {
+		return getAttr(ConstantWebContext.request_localePram);
+	}
+
+	/**
+	 * 获取当前国际化资源Map
+	 * @return
+	 */
+//	protected Map<String, String> getI18nMap() {
+//		String localePram = getAttr(ConstantWebContext.request_localePram);
+//		return I18NPlugin.get(localePram);
+//	}
+
+	/**
+	 * 获取当前国际化资源值
+	 * @return
+	 */
+//	protected String getI18nVal(String key) {
+//		Map<String, String> i18nMap = getI18nMap();
+//		return i18nMap.get(key);
+//	}
+
+	/**
+	 * 根据当前国际化查询字段扩展名
+	 * @param i18n
+	 * @return
+	 */
+//	protected String geti18nColumnSuffix(){
+//		String localePram = getAttr(ConstantWebContext.request_localePram);
+//		return I18NPlugin.columnSuffix(localePram);
+//	}
+	
+	/**
+	 * 获取项目请求根路径
+	 * @return
+	 */
+	protected String getCxt() {
+		return getAttr(ConstantWebContext.request_cxt);
+	}
+
+	/**
+	 * 获取当前用户id
+	 * @return
+	 */
+	protected String getCUserIds(){
+		return getAttr(ConstantWebContext.request_cUserIds);
+	}
+
+	/**
+	 * 获取当前用户
+	 * @return
+	 */
+	protected User getCUser(){
+		return getAttr(ConstantWebContext.request_cUser);
+	}
+
+	/**
+	 * 获取ParamMap
+	 * @return
+	 */
+	protected Map<String, String> getParamMap(){
+		return getAttr(ConstantWebContext.request_paramMap);
+	}
+
+	/**
+	 * 添加值到ParamMap
+	 * @return
+	 */
+	protected void addToParamMap(String key, String value){
+		Map<String, String> map = getAttr(ConstantWebContext.request_paramMap);
+		map.put(key, value);
+	}
+
+	/**
+	 * request流转字符串
+	 * @return
+	 */
+	protected String getRequestContent(){
+		return ToolWeb.requestStream(getRequest());
+	}
+	
+	/**
+	 * 重写getPara，进行二次decode解码
+	 */
+	@Override
+	public String getPara(String name) {
+		if ("GET".equalsIgnoreCase(getRequest().getMethod().toUpperCase())){
+			return ToolWeb.getParam(getRequest(), name);
+		}
+		return super.getPara(name);
+	}
+	
+	/**
+	 * 重写renderJson，避免出现IE8下出现下载弹出框
+	 */
+	@Override
+	public void renderJson(Object object) {
+		String userAgent = getRequest().getHeader("User-Agent");
+		if(userAgent.toLowerCase().indexOf("msie") != -1){
+			render(new JsonRender(object).forIE());
+		}else{
+			super.renderJson(object);
+		}
+	}
+
+	/**
+	 * 解决IE8下下载失败的问题
+	 */
+	@Override
+	public void renderFile(String file) {
+		String userAgent = getRequest().getHeader("User-Agent");
+		if(userAgent.toLowerCase().indexOf("msie") != -1){
+			getResponse().reset(); 
+		}
+		super.renderFile(file);
+	}
+
+	/**
+	 * 解决IE8下下载失败的问题
+	 */
+	@Override
+	public void renderFile(File file) {
+		String userAgent = getRequest().getHeader("User-Agent");
+		if(userAgent.toLowerCase().indexOf("msie") != -1){
+			getResponse().reset(); 
+		}
+		super.renderFile(file);
+	}
+
+
+	/**
+	 * 表单数组映射List<Model>
+	 * @param modelClass
+	 * @return
+	 */
+	public <T extends BaseModel<T>> List<T> getModels(Class<? extends T> modelClass){
+		String modelName = modelClass.getSimpleName();
+		String prefix = StrKit.firstCharToLowerCase(modelName) + "List";
+		return getModels(modelClass, prefix, false);
+	}
+
+	/**
+	 * 表单数组映射List<Model>
+	 * @param modelClass
+	 * @param skipConvertError
+	 * @return
+	 */
+	public <T extends BaseModel<T>> List<T> getModels(Class<? extends T> modelClass, boolean skipConvertError){
+		String modelName = modelClass.getSimpleName();
+		String prefix = StrKit.firstCharToLowerCase(modelName) + "List";
+		return getModels(modelClass, prefix, skipConvertError);
+	}
+	
+	/**
+	 * 表单数组映射List<Model>
+	 * @param modelClass
+	 * @param prefix
+	 * @return
+	 */
+	public <T extends BaseModel<T>> List<T> getModels(Class<? extends T> modelClass, String prefix){
+		return getModels(modelClass, prefix, false);
+	}
+	
+	/**
+	 * 表单数组映射List<Model>
+	 * @param modelClass
+	 * @param prefix
+	 * 
+	 * 描述：
+	 * 		
+	 * 		表单	
+	 *		<input type="hidden" name="groupList[0].ids" value="111"/>
+	 *		<input type="text" name="groupList[0].names" value="222"/>
+	 *		
+	 *		<input type="hidden" name="groupList[1].ids" value="333"/>
+	 *		<input type="text" name="groupList[1].names" value="444"/>
+	 *		
+	 *		<input type="hidden" name="groupList[3].ids" value="555"/>
+	 *		<input type="text" name="groupList[3].names" value="666"/>
+	 * 
+	 * 		调用方法 
+	 * 		List<Group> groupList = ToolModelInjector.injectModels(getRequest(), Group.class, "groupList");
+	 * 		
+	 * 		// 默认的prefix是Model类的首字母小写加上List
+	 * 		List<Group> groupList = ToolModelInjector.injectModels(getRequest(), Group.class); 
+	 */
+	public <T extends BaseModel<T>> List<T> getModels(Class<? extends T> modelClass, String prefix, boolean skipConvertError){
+		int maxIndex = 0;	// 最大的数组索引
+		boolean zeroIndex = false; // 是否存在0索引
+		
+		String arrayPrefix = prefix + "[";
+		String key = null;
+		Enumeration<String> names = getRequest().getParameterNames();
+		while (names.hasMoreElements()) {
+			key = names.nextElement();
+			if (key.startsWith(arrayPrefix) && key.indexOf("]") != -1) {
+				int indexTemp = Integer.parseInt(key.substring(key.indexOf("[") + 1, key.indexOf("]")));
+				
+				if(indexTemp == 0){
+					zeroIndex = true; // 是否存在0索引
+				} 
+				
+				if(indexTemp > maxIndex){
+					maxIndex = indexTemp; // 找到最大的数组索引
+				}
+			}
+		}
+		
+		List<T> modelList = new ArrayList<T>();
+		for (int i = 0; i <= maxIndex; i++) {
+			if((i == 0 && zeroIndex) || i != 0){ // 避免表单空值时调用产生一个无用的值
+				T baseModel = (T) getModel(modelClass, prefix + "[" + i + "]", skipConvertError);
+				modelList.add(baseModel);
+			}
+		}
+		
+		return modelList;
+	}
+	
+	/**
+	 * 表单数组映射List<Bean>
+	 * @param beanClass
+	 * @return
+	 */
+	public <T> List<T> getBeans(Class<T> beanClass){
+		String modelName = beanClass.getSimpleName();
+		String prefix = StrKit.firstCharToLowerCase(modelName) + "List";
+		return getBeans(beanClass, prefix, false);
+	}
+
+	/**
+	 * 表单数组映射List<Bean>
+	 * @param beanClass
+	 * @param skipConvertError
+	 * @return
+	 */
+	public <T> List<T> getBeans(Class<T> beanClass, boolean skipConvertError){
+		String modelName = beanClass.getSimpleName();
+		String prefix = StrKit.firstCharToLowerCase(modelName) + "List";
+		return getBeans(beanClass, prefix, skipConvertError);
+	}
+	
+	/**
+	 * 表单数组映射List<Bean>
+	 * @param beanClass
+	 * @param prefix
+	 * @return
+	 */
+	public <T> List<T> getBeans(Class<T> beanClass, String prefix){
+		return getBeans(beanClass, prefix, false);
+	}
+	
+	/**
+	 * 表单数组映射List<Bean>
+	 * @param beanClass
+	 * @param prefix
+	 */
+	public <T> List<T> getBeans(Class<T> beanClass, String prefix, boolean skipConvertError){
+		int maxIndex = 0;	// 最大的数组索引
+		boolean zeroIndex = false; // 是否存在0索引
+		
+		String arrayPrefix = prefix + "[";
+		String key = null;
+		Enumeration<String> names = getRequest().getParameterNames();
+		while (names.hasMoreElements()) {
+			key = names.nextElement();
+			if (key.startsWith(arrayPrefix) && key.indexOf("]") != -1) {
+				int indexTemp = Integer.parseInt(key.substring(key.indexOf("[") + 1, key.indexOf("]")));
+				
+				if(indexTemp == 0){
+					zeroIndex = true; // 是否存在0索引
+				} 
+				
+				if(indexTemp > maxIndex){
+					maxIndex = indexTemp; // 找到最大的数组索引
+				}
+			}
+		}
+		
+		List<T> beanList = new ArrayList<T>();
+		for (int i = 0; i <= maxIndex; i++) {
+			if((i == 0 && zeroIndex) || i != 0){ // 避免表单空值时调用产生一个无用的值
+				T baseModel = (T) getBean(beanClass, prefix + "[" + i + "]", skipConvertError);
+				beanList.add(baseModel);
+			}
+		}
+		
+		return beanList;
+	}
+
+	/**
+	 * 表单数组映射List<Model>
+	 * @param modelClass
+	 * @return
+	 */
+	public <T extends BaseModel<T>> Controller keepModels(Class<? extends T> modelClass) {
+		String modelName = StrKit.firstCharToLowerCase(modelClass.getSimpleName());
+		return keepModels(modelClass, modelName, false);
+	}
+	
+	/**
+	 * 表单数组映射List<Model>
+	 * @param modelClass
+	 * @param skipConvertError
+	 * @return
+	 */
+	public <T extends BaseModel<T>> Controller keepModels(Class<? extends T> modelClass, boolean skipConvertError){
+		String modelName = StrKit.firstCharToLowerCase(modelClass.getSimpleName());
+		return keepModels(modelClass, modelName, skipConvertError);
+	}
+
+	/**
+	 * 表单数组映射List<Model>
+	 * @param modelClass
+	 * @param modelName
+	 * @return
+	 */
+	public <T extends BaseModel<T>> Controller keepModels(Class<? extends T> modelClass, String modelName) {
+		return keepModels(modelClass, modelName, false);
+	}
+	
+	/**
+	 * 表单数组映射List<Model>
+	 * @param modelClass
+	 * @param modelName
+	 * @param skipConvertError
+	 * @return
+	 */
+	public <T extends BaseModel<T>> Controller keepModels(Class<? extends T> modelClass, String modelName, boolean skipConvertError) {
+		if (StrKit.notBlank(modelName)) {
+			List<T> model = getModels(modelClass, modelName, skipConvertError);
+			setAttr(modelName, model);
+		} else {
+			keepPara();
+		}
+		return this;
+	}
+	
+	/**
+	 * 表单数组映射List<Bean>
+	 * @param beanClass
+	 * @return
+	 */
+	public <T> Controller keepBeans(Class<T> beanClass) {
+		String modelName = StrKit.firstCharToLowerCase(beanClass.getSimpleName());
+		return keepBeans(beanClass, modelName, false);
+	}
+	
+	/**
+	 * 表单数组映射List<Bean>
+	 * @param beanClass
+	 * @param skipConvertError
+	 * @return
+	 */
+	public <T> Controller keepBeans(Class<T> beanClass, boolean skipConvertError){
+		String modelName = StrKit.firstCharToLowerCase(beanClass.getSimpleName());
+		return keepBeans(beanClass, modelName, skipConvertError);
+	}
+
+	/**
+	 * 表单数组映射List<Bean>
+	 * @param beanClass
+	 * @param beanName
+	 * @return
+	 */
+	public <T> Controller keepBeans(Class<T> beanClass, String beanName) {
+		return keepBeans(beanClass, beanName, false);
+	}
+	
+	/**
+	 * 表单数组映射List<Bean>
+	 * @param beanClass
+	 * @param beanName
+	 * @param skipConvertError
+	 * @return
+	 */
+	public <T> Controller keepBeans(Class<T> beanClass, String beanName, boolean skipConvertError) {
+		if (StrKit.notBlank(beanName)) {
+			List<T> model = getBeans(beanClass, beanName, skipConvertError);
+			setAttr(beanName, model);
+		} else {
+			keepPara();
+		}
+		return this;
+	}
+	
+	/**
+	 * 获取checkbox值，数组
+	 * @param name
+	 * @return
+	 */
+	protected String[] getParas(String name) {
+		return getRequest().getParameterValues(name);
+	}
+	
+	
+	/**
+	 * 判断请求者和数据创建者是否一致
+	 * @param entity
+	 * @param user
+	 * @return
+	 */
+	protected <T extends BaseModel<?>>  boolean authCreate(T model){
+		String createids = model.getStr("createids");
+		if(StrKit.notBlank(createids)){
+			String cUserIds = getCUserIds();
+			if(createids.equals(cUserIds)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 获取查询参数
+	 * 说明：和分页分拣一样，但是应用场景不一样，主要是给查询导出的之类的功能使用
+	 * @return
+	 */
+	protected Map<String, String> getQueryParam(){
+		Map<String, String> queryParam = new HashMap<String, String>();
+		Enumeration<String> paramNames = getParaNames();
+		while (paramNames.hasMoreElements()) {
+			String name = paramNames.nextElement();
+			String value = getPara(name);
+			if (name.startsWith(ConstantWebContext.request_query) && StrKit.notBlank(value)) {// 查询参数分拣
+				String key = name.substring(7);
+				queryParam.put(key, value.trim());
+			}
+		}
+		
+		return queryParam;
+	}
+
+	
+
+	/**
+	 * 排序方式
+	 * 说明：和分页分拣一样，但是应用场景不一样，主要是给查询导出的之类的功能使用
+	 * @return
+	 */
+	protected String getOrderMode(){
+		String orderMode = getPara(ConstantWebContext.request_orderMode);
+		return orderMode;
+	}
+
+	/**
+	 * 分页
+	 * @param splitPage		分页对象
+	 * @param selectSqlId	select之后，from之前
+	 * @param fromSqlId		from之后
+	 */
+	protected void paging(SplitPage splitPage, String selectSqlId, String fromSqlId){
+		paging(ConstantInit.db_dataSource_main, splitPage, selectSqlId, fromSqlId);
+	}
+	
+	/**
+	 * 分页
+	 * @param dataSource 数据源
+	 * @param splitPage
+	 * @param sqlId
+	 */
+	protected void paging(String dataSource, SplitPage splitPage, String selectSqlId, String fromSqlId){
+		BaseService.paging(dataSource, splitPage, selectSqlId, fromSqlId);
+	}
+
+	/**
+	 * Distinct分页
+	 * @param splitPage			分页对象
+	 * @param sqlId				完整的分页sql语句
+	 * @param distinctSqlId		分页查询distinct语句，不包含from之后
+	 */
+	protected void pagingDistinct(SplitPage splitPage, String selectSqlId, String distinctSqlId, String fromSqlId){
+		BaseService.pagingDistinct(ConstantInit.db_dataSource_main, splitPage, selectSqlId, distinctSqlId, fromSqlId);
+	}
+
+	/**
+	 * Distinct分页
+	 * @param dataSource 		数据源
+	 * @param splitPage			分页对象
+	 * @param sqlId				完整的分页sql语句
+	 * @param distinctSqlId		分页查询distinct语句，不包含from之后
+	 */
+	protected void pagingDistinct(String dataSource, SplitPage splitPage, String selectSqlId, String distinctSqlId, String fromSqlId){
+		BaseService.pagingDistinct(dataSource, splitPage, selectSqlId, distinctSqlId, fromSqlId);
+	}
+
+	/**
+     * 获取SQL，固定SQL
+     * @param sqlId
+     * @return
+     */
+	protected String getSqlMy(String sqlId){
+		return BaseService.getSqlMy(sqlId);
+	}
+
+    /**
+     * 获取SQL，动态SQL，使用Beetl解析
+     * @param sqlId
+     * @param param
+     * @return
+     */
+	protected String getSqlByBeetl(String sqlId, Map<String, Object> param){
+    	return BaseService.getSqlByBeetl(sqlId, param);
+    }
+    
+    /**
+     * 获取SQL，动态SQL，使用Beetl解析
+     * @param sqlId 
+     * @param param 查询参数
+     * @param list 用于接收预处理的值
+     * @return
+     */
+	protected String getSqlByBeetl(String sqlId, Map<String, Object> param, LinkedList<Object> list){
+    	return BaseService.getSqlByBeetl(sqlId, param, list);
+    }
+
+	/************************************		get 	set 	方法		************************************************/
+
+
+	public void setSplitPage(SplitPage splitPage) {
+		this.splitPage = splitPage;
+	}
+
+}
