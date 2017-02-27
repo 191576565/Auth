cd3dd1dd7344873c33d19282d2edc74ec584c53c
FitzFang <fcdlutfc@hotmail.com>
2017-02-27 15:52:36 +0800

    更新用户修改和删除功能

--- AM/src/main/java/com/tianjian/auth/mvc/usrMgmt/UsrMgmtController.java
+++ AM/src/main/java/com/tianjian/auth/mvc/usrMgmt/UsrMgmtController.java
@@ -1,8 +1,7 @@
 package com.tianjian.auth.mvc.usrMgmt;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
-
 import com.jfinal.core.Controller;
 import com.jfinal.log.Log;
 import com.tianjian.auth.mvc.base.BaseSecurityMD5;
@@ -38,7 +37,6 @@ public class UsrMgmtController extends Controller {
 		renderJson();
 	}
 	public void insUsr(){
-
 		//获取form中的信息
 		String userId = getPara("scopeCode");
 		String userName = getPara("usrName");
@@ -64,7 +62,42 @@ public class UsrMgmtController extends Controller {
 		usrMgmt.set("creator", sessionUserId);
 		
 		usrMgmt.save();
-		
 		index();
+	}
+	public void updtUsr(){
+		//获取form中的信息
+		String uuid = getPara("uuid");
+		String userName = getPara("usrName");
+		//获取密码并用MD5加密
+		String userPwd = BaseSecurityMD5.encodeMD5Hex(getPara("pwd"));
+		String userPhone = getPara("phone");
+		String userEmail = getPara("email");
+		//前台没做domainUUID和orgUUID，先暂用常量代替
+		String domainUUID = "48ABB16E6BB7164DE055000000000001";
+		String orgUUID = "4930C618599C63FBE055000000000001";
+		//将session中的userid作为创建者
+		String sessionUserId = getSessionAttr("user_id");
+		
+		//将获得的字段与表字段对应
+		usrMgmt.set("uuid", uuid);
+		usrMgmt.set("domain_uuid", domainUUID);
+		usrMgmt.set("org_uuid", orgUUID);
+		usrMgmt.set("roles", null);
+		usrMgmt.set("user_pwd", userPwd);
+		usrMgmt.set("user_name", userName);
+		usrMgmt.set("user_email", userEmail);
+		usrMgmt.set("user_phone", userPhone);
+		usrMgmt.set("modifier", sessionUserId);
+		usrMgmt.set("modified_date", new Timestamp(System.currentTimeMillis()));
+		usrMgmt.update();
+		index();
+	}
+	public void delUsr(){
+		//获取form中的信息
+		String uuid = getPara("uuid");
+		//将获得的字段与表字段对应
+		usrMgmt.set("uuid", uuid);
+		
+		usrMgmt.delete();
 	}
 }
