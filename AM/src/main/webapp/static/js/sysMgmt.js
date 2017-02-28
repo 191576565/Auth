6ce12c23a1e821220846fe767505a2f9996494dd
YeQuancai <775496282@qq.com>
2017-02-28 10:01:59 +0800

    test

--- AM/src/main/webapp/static/js/sysMgmt.js
+++ AM/src/main/webapp/static/js/sysMgmt.js
@@ -18,7 +18,6 @@ $('#table').bootstrapTable({
 	minimumCountColumns: 	2, 						//最少允许的列数
 	clickToSelect: 			false, 					//是否启用点击选中行
 //	height: 				400,      				//行高，如果没有设置height属性，表格自动根据记录条数调整表格高度
-	uniqueId: 				"ID", 					//每一行的唯一标识，一般为主键列
 	showToggle: 			false, 					//是否显示详细视图和列表视图的切换按钮
 	cardView: 				false, 					//是否显示详细视图
 	detailView: 			false,     				//是否显示父子表
