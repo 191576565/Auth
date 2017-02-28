86b6ffdc06bea27f0bc23946598ffa683fc52637
laowang1 <xietao@pccqcpa.com.cn>
2017-02-28 15:39:51 +0800

    新增数据权限管理init清单数据获取

--- AM/src/main/java/com/tianjian/auth/mvc/dpgMgmt/DpgMgmtController.java
+++ AM/src/main/java/com/tianjian/auth/mvc/dpgMgmt/DpgMgmtController.java
@@ -1,21 +1,57 @@
 package com.tianjian.auth.mvc.dpgMgmt;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.jfinal.core.Controller;
 import com.jfinal.log.Log;
+import com.jfinal.plugin.activerecord.Record;
+import com.tianjian.auth.mvc.api.ApiJsonService;
 import com.tianjian.auth.mvc.dpgMgmt.DpgMgmtController;
+import com.tianjian.auth.mvc.model.DpgMgmt;
+import com.tianjian.auth.mvc.oplog.OpLog;
+import com.tianjian.auth.mvc.oplog.OpLogService;
+import com.tianjian.platform.pjson.PageJson;
 
 public class DpgMgmtController extends Controller {
 	
 	private static final Log log = Log.getLog(DpgMgmtController.class);
+	private DpgMgmtService dpgmgmtservice = new DpgMgmtService();
 	
 	public void index() {
 		log.info("jump to dpgMgmt");
 		render("dpgMgmt.jsp");
-
 	}
 	
+	 /** 
+	 *@Function 获取数据权限组管理list清单           
+	 *@Declare   init 数据权限组管理清单
+	 *@Author    谢涛
+	 *@Return    String  void
+	 */
 	public void list() {
-		log.info("jump to dpgMgmt");
-		render("dpgMgmt.jsp");
+		Map<String, Object> mpara = new HashMap<String, Object>();
+		// 获取表单参数
+		Integer pageSize = getParaToInt("pageSize");
+		Integer pageNumber = getParaToInt("pageNumber");
+ 		String user_id = getPara("user_id");
+//		String op_type = getPara("op_type");
+//		String startdate_start = getPara("startdate_start");
+//		String startdate_end = getPara("startdate_end");
+		//获取session中domain_id
+		Object userinfo = getSessionAttr("userinfo");	
+		String domain_id=((Record) userinfo).getStr("domain_id");
+		
+		//List<Record> user = dpgmgmtservice.getSelectlist(username,usersession,type);
+		// 组装sql参数
+		//mpara.put("user_id", user_id);
+		mpara.put("domain_id", domain_id);
+		String selectsql = dpgmgmtservice.getSelectSql(DpgMgmt.sqlId_DM_select);
+		// 获取from语句
+		String wheresql = dpgmgmtservice.getFromSql(DpgMgmt.sqlId_DM_selectfrom, mpara);
+		// 获取数据
+		PageJson<DpgMgmt> myjson = dpgmgmtservice.getPageData(pageSize, pageNumber, selectsql, wheresql);
+		renderJson(myjson);
 	}
 }
--- AM/src/main/webapp/static/js/dpgMgmt.js
+++ AM/src/main/webapp/static/js/dpgMgmt.js
@@ -1,36 +1,48 @@
-$('#table').bootstrapTable({
-    url: 'static/js/demo/8.json',
+function initdpgMgmtlist(){
+   $('#table').bootstrapTable({
+	url: 'dpgMgmt/list',
 // 	toolbar: '#toolbar', //工具按钮用哪个容器
  	striped: true, //是否显示行间隔色
  	pagination: true, //是否显示分页（*）
- 	sortable: false, //是否启用排序
+ 	sortable: true, //是否启用排序
  	sortOrder: "asc", //排序方式
  	pageNumber:1, //初始化加载第一页，默认第一页
- 	pageSize: 5, //每页的记录行数（*）
+ 	pageSize: 10, //每页的记录行数（*）
  	pageList: [10, 25, 50, 100], //可供选择的每页的行数（*）
 // 	strictSearch: true,
 // 	showColumns: true, //是否显示所有的列
 // 	showRefresh: true, //是否显示刷新按钮
-// 	clickToSelect: true, //是否启用点击选中行
- 	uniqueId: "ID", //每一行的唯一标识，一般为主键列
-// 	showToggle:true, //是否显示详细视图和列表视图的切换按钮
-// 	cardView: false, //是否显示详细视图
+ 	clickToSelect: true, //是否启用点击选中行
+ 	uniqueId: "group_id", //每一行的唯一标识，一般为主键列
+ 	showToggle:false, //是否显示详细视图和列表视图的切换按钮
+ 	cardView: false, //是否显示详细视图
+ 	queryParamsType: '',
+ 	sidePagination: "server",
+ 	queryParams: function queryParams(params) {   //设置查询参数  
+        var param = {    
+            pageNumber: params.pageNumber,    
+            pageSize: params.pageSize,  
+            user_id : $("#userid").val()
+            //user_id : "1"
+        };    
+        return param;                   
+      },  
 
     columns: [{
-        field: 'id',
+        field: 'domain_id',
         title: '域 ID',
         checkbox: true
     }, {
-        field: 'sys_name',
+        field: 'domain_name',
         title: '域名称'
     },{
-        field: 'gp_code',
+        field: 'group_id',
         title: '组编码'
     }, {
-        field: 'gp_name',
+        field: 'group_desc',
         title: '组名称'
     }, {
-        field: 'bl_user',
+        field: 'users',
         title: '所属用户'
     }, {
     	field: 'opt',
@@ -43,6 +55,8 @@ $('#table').bootstrapTable({
     	}
     },]
 });
+
+};
 
 //yeqc
 //layer弹出自定义div
