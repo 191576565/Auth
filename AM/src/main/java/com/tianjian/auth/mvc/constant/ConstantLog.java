6ee815f7372e7947e2d03e01411711b9a131b1cc
191576565 <191576565@qq.com>
2017-03-01 14:52:14 +0800

    1.日志操作类型汇总

--- AM/src/main/java/com/tianjian/auth/mvc/constant/ConstantLog.java
+++ AM/src/main/java/com/tianjian/auth/mvc/constant/ConstantLog.java
@@ -2,49 +2,57 @@ package com.tianjian.auth.mvc.constant;
 
 public interface ConstantLog {
 	/**
-	 * 日志  操作类型
+	 * AUTH 三层标题
 	 */
-	public static final String log_optype = "OPTYPE";
+	public static final String basemgr  =" BASEMGR";  // 基础配置
+	public static final String userauth = "USERAUTH" ; //用户权限
+	public static final String syslog   = "SYSLOG" ;   //系统日志
 	
-	/**
-	 * 日志 操作信息
-	 */
-	public static final String log_opcontent = "OPCONTENT";
 	/**
 	 * 域操作信息
 	 */	
-	public static final String dmn_add = "DMN-ADD";
-	public static final String dmn_chg = "DMN-CHG";
-	public static final String dmn_del = "DMN-DEL";	
+	public static final String dmn     = basemgr+"-DMN";
+	public static final String dmn_add = dmn+"-ADD";
+	public static final String dmn_chg = dmn+"-CHG";
+	public static final String dmn_del = dmn+"-DEL";	
 	/**
 	 * 机构操作信息
 	 */	
-	public static final String org_add = "ORG-ADD";
-	public static final String org_chg = "ORG-CHG";
-	public static final String org_del = "ORG-DEL";	
+	public static final String org     = basemgr+"-ORG";
+	public static final String org_add = org+"-ADD";
+	public static final String org_chg = org+"-CHG";
+	public static final String org_del = org+"-DEL";	
 	/**
 	 * 用户操作信息
 	 */
-	public static final String user_add = "USER-ADD";
-	public static final String user_chg = "USER-CHG";
-	public static final String user_del = "USER-DEL";	
+	public static final String user     = userauth+"-USER" ;
+	public static final String user_add = user+"-ADD";
+	public static final String user_chg = user+"-CHG";
+	public static final String user_del = user+"-DEL";	
 	/**
 	 * 角色操作信息
 	 */	
-	public static final String role_add = "ROLE-ADD";
-	public static final String role_chg = "ROLE-CHG";
-	public static final String role_del = "ROLE-DEL";	
+	public static final String role     = userauth+"-ROLE" ;
+	public static final String role_add = role+"-ADD";
+	public static final String role_chg = role+"-CHG";
+	public static final String role_del = role+"-DEL";	
 	/**
 	 * 资源操作信息
 	 */	
-	public static final String res_add = "RES-ADD";
-	public static final String res_chg = "RES-CHG";
-	public static final String res_del = "RES-DEL";	
+	public static final String res     = userauth+"RES"; 
+	public static final String res_add = res+"-ADD";
+	public static final String res_chg = res+"-CHG";
+	public static final String res_del = res+"-DEL";	
 	/**
 	 * 数据权限组操作信息
-	 */	
-	public static final String grp_add = "GRP-ADD";
-	public static final String grp_chg = "GRP-CHG";
-	public static final String grp_del = "GRP-DEL";	
-		
+	 */
+	public static final String grp      =userauth+"-GRP";	 
+	public static final String grp_add = grp+"-ADD";
+	public static final String grp_chg = grp+"-CHG";
+	public static final String grp_del = grp+"-DEL";	
+	/**
+	 * 日志  操作信息
+	 */
+	public static final String log_optype = syslog+"OPTYPE";
+	public static final String log_opcontent = syslog+"-OPCONTENT";	
 }
