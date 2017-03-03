64f7a0829adbf8a2bc9e6e47f9e8d50a9800b90b
hujian722 <hujian@pccqcpa.com.cn>
2017-03-03 10:14:54 +0800

    菜单

--- AM/src/main/java/com/tianjian/auth/mvc/resMgmt/ResMgmtController.java
+++ AM/src/main/java/com/tianjian/auth/mvc/resMgmt/ResMgmtController.java
@@ -8,78 +8,84 @@ import com.jfinal.log.Log;
 import com.jfinal.plugin.activerecord.Record;
 
 public class ResMgmtController extends Controller {
-	
+
 	private static final Log log = Log.getLog(ResMgmtController.class);
 	private ResMgmtService reservice = new ResMgmtService();
-	
+
 	public void index() {
 		render("resMgmt.jsp");
 	}
+
 	/*
 	 * 资源的获取
 	 */
 	public void get() {
-	
-		List<ResMgmt> all=reservice.get(ResMgmt.sqlId_res_select);
+
+		List<ResMgmt> all = reservice.get(ResMgmt.sqlId_res_select);
 		renderJson(all);
 	}
+
 	public void save() {
-		ResMgmt res=getModel(ResMgmt.class,"res");
+		ResMgmt res = getModel(ResMgmt.class, "res");
+
+		// res.set("domain_uuid", "hujian");
+		res.set(ResMgmt.column_created_date, new Timestamp(System.currentTimeMillis()));
+		// res.set("creator", "hujian");
+		// 获取session中domain_id
+		Object userinfo = getSessionAttr("userinfo");
+		String userid = ((Record) userinfo).getStr("user_id");
+		res.set(ResMgmt.column_created_date, new Timestamp(System.currentTimeMillis()));
+		res.set(ResMgmt.column_creator, userid);
 		
-		res.set("domain_uuid", "hujian");
-		res.set("created_date", new Timestamp(System.currentTimeMillis()));
-		res.set("creator", "hujian");
-		res.set("modified_date", new Timestamp(System.currentTimeMillis()));
-		res.set("modifier", "hujian");
-		System.out.println("res---"+res);
-		boolean savesucess=res.save();
-		if (savesucess){
+		boolean savesucess = res.save();
+		if (savesucess) {
 			renderJson(true);
-		}else{
+		} else {
 			renderJson(false);
 		}
-		
+
 	}
+
 	public void put() {
-		ResMgmt res=getModel(ResMgmt.class,"res");
-		
+		ResMgmt res = getModel(ResMgmt.class, "res");
+
 		res.set("domain_uuid", "hujian");
 		res.set("modified_date", new Timestamp(System.currentTimeMillis()));
 		res.set("modifier", "hujian");
-		System.out.println("res---"+res);
-		boolean savesucess=res.update();
-		if (savesucess){
+		System.out.println("res---" + res);
+		boolean savesucess = res.update();
+		if (savesucess) {
 			renderJson(true);
-		}else{
+		} else {
 			renderJson(false);
 		}
-		
+
 	}
+
 	public void delete() {
-		ResMgmt res=new ResMgmt();
-		
+		ResMgmt res = new ResMgmt();
+
 		res.set("uuid", getPara("uuid"));
-		boolean savesucess=res.delete();
-		if (savesucess){
+		boolean savesucess = res.delete();
+		if (savesucess) {
 			renderJson(true);
-		}else{
+		} else {
 			renderJson(false);
 		}
-		
+
 	}
-	
+
 	/*
-	 * 根据res_type，res_up_uuid，user_id获取菜单
-	 * author： hujian
+	 * 根据res_type，res_up_uuid，user_id获取菜单 author： hujian
 	 * 
 	 */
 	public void menuPage() {
 		String restype = getPara("res_type");
 		String resupuuid = getPara("res_up_uuid");
-		Object userinfo = getSessionAttr("userinfo");	
-		String userid=((Record) userinfo).getStr("user_id");
-		System.out.println(restype+"-"+resupuuid+"-"+userid);
-		List<Record> menupage=reservice.getMenuPage(ResMgmt.sqlId_resmenu_select,userid,resupuuid,restype);
+		Object userinfo = getSessionAttr("userinfo");
+		String userid = ((Record) userinfo).getStr("user_id");
+		System.out.println(restype + "-" + resupuuid + "-" + userid);
+		List<Record> menupage = reservice.getMenuPage(ResMgmt.sqlId_resmenu_select, userid, resupuuid, restype);
 		renderJson(menupage);
 	}
 }
