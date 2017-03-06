692d937f4daf10fb345879581c6fd5aa3f8a016a
laowang1 <xietao@pccqcpa.com.cn>
2017-03-06 13:52:53 +0800

    jfinal ajax跨域的解决办法 测试，commit：5

--- AM/src/main/java/com/tianjian/auth/mvc/handler/GlobalHandler.java
+++ AM/src/main/java/com/tianjian/auth/mvc/handler/GlobalHandler.java
@@ -26,6 +26,7 @@ public class GlobalHandler extends Handler {
 	    	 //   int index = target.lastIndexOf(".jsp");
 	     	//    if (index != -1)
 	     	//      target = target.substring(0, index);
+	     	response.addHeader("Access-Control-Allow-Origin", "*");
 		    nextHandler.handle(target, request, response, isHandled);
 		  }
 		 
