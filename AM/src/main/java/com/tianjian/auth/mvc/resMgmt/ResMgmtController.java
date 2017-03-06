169490edd09102d7903854939b80e5ef431eba3f
yangshuang <806312852@qq.com>
2017-03-06 15:32:13 +0800

    no message

--- AM/src/main/java/com/tianjian/auth/mvc/resMgmt/ResMgmtController.java
+++ AM/src/main/java/com/tianjian/auth/mvc/resMgmt/ResMgmtController.java
@@ -89,7 +89,6 @@ public class ResMgmtController extends Controller {
 		} else {
 			renderJson(false);
 		}
-
 	}
 	
 	/*
--- AM/src/main/webapp/static/js/resMgmt.js
+++ AM/src/main/webapp/static/js/resMgmt.js
@@ -1,231 +1,169 @@
+var s = {
+	view: {
+		dblClickExpand: false,
+		selectedMulti: false
+	},
+	data: {
+		simpleData: {
+			enable: true,
+			idKey: "id",
+			pIdKey: "pid",
+			rootPId: '#'
+		}
+	},
+	callback: {
+		onDblClick: zTreeOnDblClickSimple,
+		onClick: zTreeOnClickSimple
+	}
+};
+function zTreeOnDblClickSimple(event, treeId, treeNode) {
+//	if(treeNode.id===$('#res_id').val()){
+//		toastr.warning('资源编码与上级资源编码冲突');
+//		return;
+//	}
+	$('#res_up_uuid').val(treeNode.id);
+	$('#up_res_name').val(treeNode.name);
+	
+	var index = layer.index; //获取当前弹层的索引号
+	layer.close(index); //关闭当前弹层
+};
+
+function zTreeOnClickSimple(event, treeId, treeNode) {
+	var treeObj = $.fn.zTree.getZTreeObj(treeId);
+	treeObj.expandNode(treeNode, null, null, null);
+
+	return true;
+};
+
+
+var rs;
 $(function(){
-	initTable()
+	initTable();
+	
+	$('.tree').click(function(){
+		
+		$.get('resMgmt/restree', function(data){
+			$.fn.zTree.init($("#res"), s, data); //树
+			
+			layer.open({
+				type : 1,
+				content : $('.t'),
+				title : '选择上级资源',
+				area : [ '400px', '400px' ],
+				maxmin: true
+			});
+		})
+	})
 })
 
 function initTable() {
-			$.get('resMgmt/get', {r:Math.random()*10000000000000}, function(data){
-				var rs=data;
-				$('#table tbody').html('');
-				var temp='';
-				if(rs!=null){
-					rs.forEach(function(e){
-						if(e.lvl===1){
-							temp='<tr class="treegrid-'+e.uuid+'" lvl="'+e.lvl+'"><td style="width:40px;text-align:center;"><input type="radio" name="orgRadio"/></td><td id="'+e.res_id+'">'+e.res_id+'</td><td>'+e.res_name+'</td><td>'
-									+e.up_res_name+'</td><td>'+e.res_url+'</td><td>'+e.res_type+'</td><td>'+e.res_class+'</td><td>'+e.res_color+'</td><td>'+e.res_icon
-									+'</td><td>-</td></tr>';
-							$('#table tbody').append(temp);
-						}else {
-							temp='<tr class="treegrid-'+e.uuid+' treegrid-parent-'+e.res_up_uuid+'" lvl="'+e.lvl+'"><td style="width:40px;text-align:center;"><input type="radio" name="orgRadio"/></td><td id="'+e.res_id+'">'+e.res_id+'</td><td>'+e.res_name+'</td><td>'
-									+e.up_res_name+'</td><td>'+e.res_url+'</td><td>'+e.res_type+'</td><td>'+e.res_class+'</td><td>'+e.res_color+'</td><td>'+e.res_icon
-									+'</td><td><button id="" type="button" class="btn btn-info btn-sm">编辑</button>&nbsp;<button id="" type="button" class="btn btn-danger btn-sm">删除</button></td></tr>';
-							$('#table tbody tr.treegrid-'+e.res_up_uuid).after(temp);
-						}
-					});
+	$.get('resMgmt/get', {r:Math.random()*10000000000000}, function(data){
+		rs=data;
+		$('#table tbody').html('');
+		var temp='';
+		if(rs!=null){
+			rs.forEach(function(e, index){
+				if(e.lvl===1){
+					temp='<tr class="treegrid-'+e.uuid+'" lvl="'+e.lvl+'" index="'+index+'"><td style="width:40px;text-align:center;"><input type="radio" name="radio"/></td><td id="'+e.res_id+'">'+e.res_id+'</td><td>'+e.res_name+'</td><td>'
+							+e.up_res_name+'</td><td>'+e.res_url+'</td><td>'+e.res_type_name+'</td><td>'+e.res_class+'</td><td>'+e.res_color+'</td><td>'+e.res_icon
+							+'</td><td>-</td></tr>';
+					temp=temp.replace(/null/g, '-');
+					$('#table tbody').append(temp);
+				}else {
+					temp='<tr class="treegrid-'+e.uuid+' treegrid-parent-'+e.res_up_uuid+'" lvl="'+e.lvl+'" index="'+index+'"><td style="width:40px;text-align:center;"><input type="radio" name="radio"/></td><td id="'+e.res_id+'">'+e.res_id+'</td><td>'+e.res_name+'</td><td>'
+							+e.up_res_name+'</td><td>'+e.res_url+'</td><td>'+e.res_type_name+'</td><td>'+e.res_class+'</td><td>'+e.res_color+'</td><td>'+e.res_icon
+							+'</td><td><button type="button" class="btn btn-info btn-sm" onclick="update(this)"  index="'+index+'">编辑</button></td></tr>';
+					temp=	temp.replace(/null/g, '-');
+					$('#table tbody tr.treegrid-'+e.res_up_uuid).after(temp);
 				}
-				
-				$('#table').treegrid({
-					treeColumn:1,
-	                expanderExpandedClass: 'glyphicon glyphicon-minus',
-	                expanderCollapsedClass: 'glyphicon glyphicon-plus'
-	            });
-				//收起第二级及一下的机构
-            		$('#table tbody tr[lvl=2]').treegrid('collapseRecursive');
-            		
-            		//tr点击单选钮选中
-	            $('#table tbody tr').click(function(){
-	            		$(this).find('input:radio')[0].checked=true;
-	            });
 			});
 		}
+		
+		$('#table').treegrid({
+			treeColumn:1,
+            expanderExpandedClass: 'glyphicon glyphicon-minus',
+            expanderCollapsedClass: 'glyphicon glyphicon-plus'
+        });
+		//收起第二级及一下的机构
+    		$('#table tbody tr[lvl=3]').treegrid('collapseRecursive');
+    		
+    		//tr点击单选钮选中
+        $('#table tbody tr').click(function(){
+        		$(this).find('input:radio')[0].checked=true;
+        });
+	});
+}
 
-//$('#table')
-//		.bootstrapTable(
-//				{
-//					url : 'resMgmt/get',
-//					toolbar : '#toolbar', // 工具按钮用哪个容器
-//					striped : true, // 是否显示行间隔色
-//					pagination : true, // 是否显示分页（*）
-//					sortable : false, // 是否启用排序
-//					sortOrder : "asc", // 排序方式
-//					pageNumber : 1, // 初始化加载第一页，默认第一页
-//					pageSize : 10, // 每页的记录行数（*）
-//					pageList : [ 10, 25, 50, 100 ], // 可供选择的每页的行数（*）
-//					strictSearch : true,
-//					showColumns : false, // 是否显示所有的列
-//					showRefresh : false, // 是否显示刷新按钮
-//					clickToSelect : false, // 是否启用点击选中行
-//					uniqueId : "ID", // 每一行的唯一标识，一般为主键列
-//					showToggle : false, // 是否显示详细视图和列表视图的切换按钮
-//					cardView : false, // 是否显示详细视图
-//
-//					columns : [
-//							{								
-//								checkbox : true
-//							},
-//							{
-//								field : 'res_id',
-//								title : '资源编码'
-//							},
-//							{
-//								field : 'res_name',
-//								title : '资源名称'
-//							},
-//							{
-//								field : 'up_res_name',
-//								title : '上级资源名称'
-//							},
-//							{
-//								field : 'res_url',
-//								title : '资源URL'
-//							},
-//							{
-//								field : 'res_type',
-//								title : '资源类型'
-//							},
-//							{
-//								field : 'res_class',
-//								title : '资源CSS'
-//							},
-//							{
-//								field : 'res_color',
-//								title : '资源颜色'
-//							},
-//							{
-//								field : 'res_icon',
-//								title : '资源ICON'
-//							},
-//							{
-//								field : 'sort_id',
-//								title : '排序编号'
-//							},
-//							{
-//								field : 'opt',
-//								title : '操 作',
-//								events : 'operateEvents',
-//								formatter : function(value, row, index) {
-//									var e = '<a href="#"  class="btn btn-info edit">编辑</a> ';
-//									var d = '<a href="#"  class="btn btn-danger delete">删除</a> ';
-//									var f = '<a href="2.html" onclick="del(\''
-//											+ row.id + '\')">角色列表</a> ';
-//									return e + d;
-//								}
-//							}, ]
-//				});
+$('#res_add').on('click', function() {
+	$('#form')[0].reset();
+	$('#uuid').val('');
+	
+	var index=$('td input:checked').parent().parent().attr('index');
+	if(index){
+		var info=rs[index];
+		
+		$('#res_up_uuid').val(info.uuid);
+		$('#up_res_name').val(info.res_name);
+	}
+	
+	layer.open({
+		type : 1,
+		content : $('#sys_add_div'),
+		// skin: 'layui-layer-molv',
+		title : '资源信息-新增',
+		area : [ '640px', '400px' ],
+		maxmin: true
+	});
+});
 
-//$('#res_add').on('click', function() {
-//	layer.open({
-//		type : 1,
-//		content : $('#sys_add_div'),
-//		// skin: 'layui-layer-molv',
-//		title : '资源信息-新增',
-//		area : [ '640px', '360px' ],
-//	// maxmin: true
-//	});
-//});
-//
-//window.operateEvents = {
-//	'click .edit' : function(e, value, row, index) {
-//		// 修改操作
-//		var $uuid = $("#sys_add_div #form #uuid").val(row.uuid);
-//		var $res_id = $("#sys_add_div #form #res_id").val(row.res_id);
-//		var $res_name = $("#sys_add_div #form #res_name").val(row.res_name);
-//
-//		var $res_up_uuid = $("#sys_add_div #form #res_up_uuid").val(row.res_up_uuid);
-//		var $res_url = $("#sys_add_div #form #res_url").val(row.res_url);
-//		var $res_class = $("#sys_add_div #form #res_class").val(row.res_class);
-//		var $res_icon = $("#sys_add_div #form #res_icon").val(row.res_icon);
-//		var $sort_id = $("#sys_add_div #form #sort_id").val(row.sort_id);
-//		console.log(row.res_name);
-//
-//		layer.open({
-//			type : 1,
-//			content : $('#sys_add_div'),
-//			title : '资源信息-编辑',
-//			area : [ '640px', '360px' ],
-//		});
-//	},
-//	'click .delete' : function(e, value, row, index) {
-//		// 删除操作
-//		console.log(row.uuid);
-//		var $deluuid = $("#sys_del_div #del_form #deluuid").val(row.uuid);
-//		layer.open({
-//			type : 1,
-//			content : $('#sys_del_div'),
-//			title : '资源信息-删除',
-//			area : [ '640px', '360px' ],
-//		});
-//	}
-//}
-//
-//$('#resdelete').click(function() {
-//	
-//		$("#del_form").attr("action", "resMgmt/delete");
-//		$('#del_form').submit(function() {
-//			$(this).ajaxSubmit(function(resultJson) {
-//				// 回调操作
-//				if (JSON.stringify(resultJson) == "false") {
-//					console.log("resultJson==false")
-//					layer.open({
-//						type : 1,
-//						content : '删除失败!',
-//						title : '删除失败',
-//						area : [ '200px', '200px' ],
-//					});
-//					return false;
-//				} else {
-//					console.log("resultJson==true")
-//					window.location.href = 'resMgmt';
-//				}
-//			});
-//
-//			return false;// 阻止表单默认提交
-//		});
-//		
-//});
-//
-//$('#resave').click(function() {
-//	// 新增操作
-//	if($("#sys_add_div #form #uuid").val() == ''){
-//	$("#form").attr("action", "resMgmt/save");
-//	$('#form').submit(function() {
-//		$(this).ajaxSubmit(function(resultJson) {
-//
-//			// 回调操作
-//			if (JSON.stringify(resultJson) == "false") {
-//				console.log("resultJson==false")
-//				layer.open({
-//					type : 1,
-//					content : '角色编码/角色名重复，新增失败!',
-//					title : '新增失败',
-//					area : [ '200px', '200px' ],
-//				});
-//				return false;
-//			} else {
-//				console.log("resultJson==true")
-//				window.location.href = 'resMgmt';
-//			}
-//		});
-//
-//		return false;// 阻止表单默认提交
-//	});
-//	}
-//	//修改操作
-//	if($("#sys_add_div #form #uuid").val() != ''){
-//		$("#form").attr("action", "resMgmt/put");
-//		$('#form').submit(function(){
-//			$(this).ajaxSubmit(function(resultJson){
-//				if(JSON.stringify(resultJson) == "true"){
-//					window.location.href='resMgmt';				
-//				}else{
-//					layer.open({
-//						type : 1,
-//						content : '资源信息-编辑',
-//						title : '编辑失败',
-//						area : [ '200px', '200px' ],
-//					});
-//				}
-//			});
-//			return false;//阻止表单默认提交
-//		});
-//	}
-//});
+function update(obj){
+	var info=rs[$(obj).attr('index')];
+	
+	for(var key in info){
+		$('#'+key).val(info[key]);
+	}
+	
+	layer.open({
+		type : 1,
+		content : $('#sys_add_div'),
+		// skin: 'layui-layer-molv',
+		title : '资源信息-编辑',
+		area : [ '640px', '400px' ],
+		maxmin: true
+	});
+}
+
+$('.save').click(function(){
+	var data=$('#form').serialize();
+	if($('#uuid').val()==''){ //add
+		$.post('resMgmt/save?'+data, function(d){
+			
+		});
+	}else { //update
+		$.post('resMgmt/put?'+data, function(d){
+			
+		});
+	}
+});
+
+$('#res_del').click(function(){
+	if($('td input:checked').length===0){
+		layer.msg('请选择要删除的资源');
+		return;
+	}
+	var index=$('td input:checked').parent().parent().attr('index');
+	layer.confirm('是否删除该资源及其子资源？', {
+	  btn: ['删除','取消'] //按钮
+	}, function(){
+		$.post('resMgmt/delete?uuid='+rs[index].uuid, function(d){
+			if(d){
+				layer.msg('资源删除成功');
+			}else {
+				layer.msg('资源删除失败');
+			}
+			initTable();
+		});
+	}, function(){
+		layer.close(layer.index);
+	});
+});
