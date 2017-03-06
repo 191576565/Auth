1babf1defd47e5f5089699d929ae07c1f96b6c81
YeQuancai <775496282@qq.com>
2017-03-06 10:48:34 +0800

    机构新增

--- AM/src/main/webapp/static/js/orgMgmt.js
+++ AM/src/main/webapp/static/js/orgMgmt.js
@@ -12,7 +12,7 @@ function initTable() {
 						if(e.level===1){
 							temp='<tr class="treegrid-'+e.uuid+'" lvl="'+e.level+'"><td>'+e.org_unit_id+'</td><td>'+e.domain_name+'</td><td>'
 									+e.org_unit_desc+'</td><td>'+e.up_org_unit_desc
-									+'</td><td>-</td></tr>';
+									+'</td><td><button id="" type="button" class="btn btn-info btn-sm">编辑</button>&nbsp;<button id="" type="button" class="btn btn-danger btn-sm">删除</button></td></tr>';
 							$('#table tbody').append(temp);
 						}else {
 							temp='<tr class="treegrid-'+e.uuid+' treegrid-parent-'+e.org_up_uuid+'" lvl="'+e.level+'"><td>'+e.org_unit_id+'</td><td>'+e.domain_name+'</td><td>'
@@ -40,3 +40,58 @@ angular.module('myApp', [])
 	$scope.userdata = {};
 	$scope.submitForm = function(){};
 })
+
+//新增机构_弹出层
+$('#org_add').on('click', function() {
+	//初始化表单
+	$("#org_add_div #form #up_org").html("");
+	//获取域
+	$.getJSON("orgMgmt/getId",function(data){
+		$.each(data, function(i, item){
+			$.each(item, function(key,value){
+				if(key === "domain_name"){
+					var $scop_n = $("#org_add_div #form #scop_n").val(value);
+				}
+				if(key === "uuid"){
+					var $uuid = $("#org_add_div #form #uuid").val(value);
+				}
+			})
+		})
+	})
+	//获取机构
+	$.getJSON("orgMgmt/getOrg",function(data){
+		console.log(data);
+		$.each(data, function(i, item){
+			var up_id = '';
+			$.each(item, function(key,value){
+				if(key === "uuid"){
+					up_id = value;
+				}
+			})
+			$.each(item, function(key,value){
+				if(key === "org_unit_desc"){
+					$('#up_org').append("<option value="+up_id+">" + value + "</option>");
+				}
+			})
+		})
+	})
+	layer.open({
+		type: 1,
+		content: $('#org_add_div'),
+		title: '机构信息',
+		area: ['960px', '540px'],
+	});
+	return false;
+});
+//新增机构_保存
+$('#sub').click(function(){
+	$("#form").attr("action", "orgMgmt/save");
+	$('#form').ajaxSubmit(function(resultJson){
+		if(JSON.stringify(resultJson) == "false"){
+			alert("机构编码不能重复!");
+			return;
+		}
+		window.location.href='orgMgmt';
+	});
+	return false;//阻止表单默认提交
+});
