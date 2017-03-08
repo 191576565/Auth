da827e37efa446b0d0adb9ceac62e77156a9ee85
YeQuancai <775496282@qq.com>
2017-03-08 11:19:47 +0800

    菜单权限第一层

--- AM/src/main/webapp/static/js/login_after.js
+++ AM/src/main/webapp/static/js/login_after.js
@@ -1,3 +1,15 @@
-$.getJSON("getMenu",function(data){
-
+$.getJSON("getMenu",function(content){
+	var resIcon = '';
+	var resUrl = '';
+	$.each(content, function(index, data){//遍历对象数组
+		$.each(data, function(key, value){
+			if(key === "res_icon"){
+				resIcon = value;
+			}
+			if(key === "res_url"){
+				resUrl = value;
+			}
+		})
+		$('#1 #2 #3 #4').append('<div class="post-masonry col-md-4 col-sm-6"><div class="post-thumb"><a href="'+resUrl+'"><img src="'+resIcon+'"></a></div></div>');
+	})
 })
